46d66f17694272ea830795d406df7b624f8b7e10
Viktoriia Kaspryk <kasprikviktoria4@gmail.com>
2025-07-13 20:23:36 +0200

    endpoint get project

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,22 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Project } from '@prisma/client'; // This is the Prisma interface/type
 import { PrismaService } from 'prisma/prisma.service';
-import { ProjectDto } from './dto/project.dto';
 
 @Injectable()
 export class ProjectsService {
   constructor(private prisma: PrismaService) {}
 
-  async findAllProjects(): Promise<ProjectDto[]> {
-    const projects = await this.prisma.project.findMany();
-    return projects.map((project) => ({
-      id: project.id,
-      projectName: project.projectName,
-      description: project.description,
-      githubLink: project.githubLink ?? null,
-      websiteLink: project.websiteLink ?? null,
-    }));
+  async findAllProjects(): Promise<Project[]> {
+    return this.prisma.project.findMany();
   }
-  // findAll() {
-  //   return this.prisma.project.findMany();
-  // }
 }
