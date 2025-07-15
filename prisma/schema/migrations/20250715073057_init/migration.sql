c6ca517f6a38453993a3e9171f7ac0079419c823
w-wachel <114748471+w-wachel@users.noreply.github.com>
2025-07-15 09:52:15 +0200

    deleted unnecessary migrations (#10)

--- prisma/schema/migrations/20250715073057_init/migration.sql
+++ prisma/schema/migrations/20250715073057_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `idUser` to the `Project` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Project" ADD COLUMN     "idUser" INTEGER NOT NULL;
-
 -- CreateTable
 CREATE TABLE "Language" (
     "id" SERIAL NOT NULL,
@@ -17,16 +8,36 @@ CREATE TABLE "Language" (
 );
 
 -- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "idUser" INTEGER NOT NULL,
+    "projectName" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "githubLink" TEXT,
+    "websiteLink" TEXT,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
+    "linkedinId" TEXT,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "description" TEXT,
+    "job" TEXT,
     "gitUrl" TEXT,
     "linkedinUrl" TEXT,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateIndex
+CREATE UNIQUE INDEX "User_linkedinId_key" ON "User"("linkedinId");
+
 -- AddForeignKey
 ALTER TABLE "Language" ADD CONSTRAINT "Language_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Project" ADD CONSTRAINT "Project_idUser_fkey" FOREIGN KEY ("idUser") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
