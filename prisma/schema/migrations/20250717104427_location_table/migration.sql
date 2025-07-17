9e18b0e55b72683bf36cef6d649cd0658a7fa85e
Wiktoria Wachel <wiktoriawachel04@gmail.com>
2025-07-17 12:48:38 +0200

    fixing locations table and user.prisma

---
+++ prisma/schema/migrations/20250717104427_location_table/migration.sql
@@ -0,0 +1,13 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "locationId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "Location" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE SET NULL ON UPDATE CASCADE;
