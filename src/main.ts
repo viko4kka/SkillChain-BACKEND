b026b8c8f3878b6e35f07daec3ffe3374f2b00b8
Wiktoria Wachel <wiktoriawachel04@gmail.com>
2025-07-23 09:52:21 +0200

    changed origin url

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   app.use(
