e98847eff55298f01af544aeae1dcc15184c9c5c
Szooot <michalos277@gmail.com>
2025-07-28 12:42:07 +0200

    feat: add logout endpoint & add cors for frontend

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import {
   Get,
   Res,
   Query,
+  Post,
   Session,
   UseGuards,
   BadRequestException,
@@ -10,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { randomUUID } from 'crypto';
 import { SessionData } from 'express-session';
+import { Session as ExpressSession } from 'express-session';
 import { Response } from 'express';
 import { LinkedInUserInfo } from './interfaces/linkedinUserInfo.interface';
 import { AuthGuard } from './guards/auth.guard';
@@ -48,7 +50,10 @@ export class AuthController {
     @Query('code') code: string,
     @Query('state') state: string,
     @Session() session: SessionData,
+    @Res() res: Response,
   ) {
+    const frontendUrl = this.configService.get<string>('FRONTEND_URL') || 'http://localhost:3000/';
+
     if (state !== session.linkedinState) {
       throw new BadRequestException('Invalid state parameter');
     }
@@ -67,7 +72,7 @@ export class AuthController {
 
       session.user = user;
 
-      return { user };
+      return res.redirect(frontendUrl + '/profile/' + user.id);
     } catch (error) {
       console.error('LinkedIn callback error:', error);
       throw new InternalServerErrorException('LinkedIn login error');
@@ -78,5 +83,17 @@ export class AuthController {
   @UseGuards(AuthGuard)
   getMe(@Session() session: SessionData) {
     return session.user;
+  }
+
+  @Post('logout')
+  @UseGuards(AuthGuard)
+  logout(@Session() session: ExpressSession & SessionData, @Res() res: Response) {
+    session.destroy(err => {
+      if (err) {
+        return res.status(500).json({ message: 'Logout failed' });
+      }
+      res.clearCookie('connect.sid');
+      return res.json({ message: 'Logged out successfully' });
+    });
   }
 }
