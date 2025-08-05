a3500638921822e561c63926030b23bd1f8fc734
viko4kka <kasprikviktoria4@gmail.com>
2025-08-06 00:40:27 +0200

    correct displayUserdto.ts

--- src/users/dto/displayUser.dto.ts
+++ src/users/dto/displayUser.dto.ts
@@ -1,28 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { UserDto } from './user.dto';
 import { SkillDto } from 'src/common/dto/skill.dto';
-import { LanguageDto } from 'src/common/dto/language.dto';
 import { LocationDto } from 'src/common/dto/location.dto';
-
-type UserSkillType = SkillDto;
-
-type UserLanguageType = LanguageDto;
-
-type UserLocationType = LocationDto;
+import { LanguageDto } from 'src/common/dto/language.dto';
 
 export class DisplayUserDto extends UserDto {
   @ApiProperty({
     description: 'Comma-separated list of user skills',
   })
-  userSkills: UserSkillType[];
+  userSkills: SkillDto[];
 
   @ApiProperty({
     description: 'Location Name of the user',
   })
-  location: UserLocationType;
+  location: LocationDto[];
 
   @ApiProperty({
     description: 'Language Name of the user',
   })
-  userLanguages: UserLanguageType[];
+  userLanguages: LanguageDto[];
 }
