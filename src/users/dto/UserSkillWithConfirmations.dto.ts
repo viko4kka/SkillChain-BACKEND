85077a55cecb558f500701c52b7f3c8521a43c6f
CAllMePiotr <paskopieter@gmail.com>
2025-08-06 13:06:39 +0200

    Update endpoint to get skills for user to get confirmations (#36)
    * Update endpoint to get skills for user to get confirmations
    
    * Updating-GetSkillsForUser_WithConfirmation
    
    * usunięcie starego endpointu + /confirmations z nowego
    
    * fix lint errors + fix description and name
    
    * + name
    
    * fix final lint error
    
    * Api Response deleate

---
+++ src/users/dto/UserSkillWithConfirmations.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserSkillWithConfirmations {
+  @ApiProperty({ description: 'Skill ID', type: Number })
+  id: number;
+
+  @ApiProperty({ description: 'Skill name', type: String })
+  name: string;
+
+  @ApiProperty({ description: 'User skill description', type: String })
+  description: string;
+
+  @ApiProperty({
+    description: 'Array of confirmations with approver data',
+    type: 'array',
+    items: {
+      type: 'object',
+      properties: {
+        id: { type: 'number' },
+        firstName: { type: 'string' },
+        lastName: { type: 'string' },
+      },
+    },
+  })
+  confirmations: {
+    id: number;
+    firstName: string;
+    lastName: string;
+  }[];
+}
